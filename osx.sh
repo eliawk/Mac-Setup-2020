d4f6c6cfb43eefb836bf6c415195a8726a3f1377
Elia Pellegrino <elia@piramid.studio>
2020-11-23 17:32:26 +0100

    edi osx

--- osx.sh
+++ osx.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # # Ask for the administrator password upfront
-# sudo -v
+sudo -v
 
-# # Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
-# while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+# Keep-alive: update existing `sudo` time stamp until `osxprep.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # install xcode CLI
 echo "Install xcode"
